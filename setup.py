a13c77008bd7eef6f6d0f13dc1487437c194db52
Sylvain Viollon <thefunny@gmail.com>
2009-04-09 17:02:19 +0000

    Print friendly page action.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='silva.pageactions.printfriendly',
+      version=version,
+      description="Display a simple printable version of the content in Silva.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva pageactions print',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva', 'silva.pageactions'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'silva.core.views  > 2.0.999, < 2.1.999',
+          'silva.pageactions.base',
+          'five.grok',
+          'megrok.chameleon',
+          ],
+      )
---
+++ silva/pageactions/printfriendly/printfriendly.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from silva.core.views import views as silvaviews
+
+class PrintFriendly(silvaviews.Template):
+    pass
