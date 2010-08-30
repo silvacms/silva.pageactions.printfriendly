964bb67a9b407eec359fc12e01efbe8aaae4e82f
Sylvain Viollon <thefunny@gmail.com>
2010-08-30 12:34:27 +0000

    2.3 update, don't need to set HTTP headers explicitly.

--- silva/pageactions/printfriendly/printfriendly.py
+++ silva/pageactions/printfriendly/printfriendly.py
@@ -3,13 +3,10 @@
 # See also LICENSE.txt
 # $Id$
 
+from five import grok
 from silva.core.layout.interfaces import ISilvaLayer, ISilvaSkin
 from silva.core.views import views as silvaviews
-from silva.core.views.interfaces import IHTTPResponseHeaders
 from silva.pageactions.base.base import PageAction
-
-from five import grok
-from zope import component
 from zope.publisher.browser import applySkin
 
 
@@ -26,12 +23,9 @@ class PrintFriendly(silvaviews.View):
     grok.name('print.html')
 
     def update(self):
-        component.getMultiAdapter(
-            (self.context, self.request), IHTTPResponseHeaders)()
         applySkin(self.request, IPrintSkin)
 
 
 class PrintAction(PageAction):
 
     grok.order(20)
-
