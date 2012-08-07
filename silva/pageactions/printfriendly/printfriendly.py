615b8d8ad6e48103df0a3ecca634a0ab84ced2b5
Sylvain Viollon <thefunny@gmail.com>
2012-08-07 16:59:28 +0200

    Update code to work with 3.0 (context/view doesn't exit anymore).

--- silva/pageactions/printfriendly/printfriendly.py
+++ silva/pageactions/printfriendly/printfriendly.py
@@ -7,7 +7,11 @@ from five import grok
 from silva.core.layout.interfaces import ISilvaLayer, ISilvaSkin
 from silva.core.views import views as silvaviews
 from silva.pageactions.base.base import PageAction
+from silva.core.views.interfaces import IView
 from zope.publisher.browser import applySkin
+from zope.component import queryMultiAdapter
+
+from zExceptions import NotFound
 
 
 class IPrintLayer(ISilvaLayer):
@@ -19,13 +23,18 @@ class IPrintSkin(IPrintLayer, ISilvaSkin):
 
 
 class PrintFriendly(silvaviews.View):
-
     grok.name('print.html')
 
     def update(self):
         applySkin(self.request, IPrintSkin)
+        self.html = None
+        view = queryMultiAdapter(
+            (self.context, self.request), name='content.html')
+        if view is None:
+            raise NotFound('content.html')
+        if IView.providedBy(view) and view.content is not None:
+            self.html = view()
 
 
 class PrintAction(PageAction):
-
     grok.order(20)
